51e7e96cd58fa814cef9aeb806edba2d553dd64b
Patryk <patryksmakosz1@gmail.com>
2020-10-03 00:15:59 +0200

    ready

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Block class="block--bottom-left" title="Górna 7/4" filename="gorna0759.jpg">
+    <p class="subtitle">Najlepsza lokalizacja w mieście</p>
+  </Block>
+  <Block title="Dostępne od zaraz" filename="gorna0769.jpg"/>
+  <Block class="block--bottom-left" filename="gorna0760.jpg">
+    <p class="subtitle">Napisz, zadzwoń 502 590 747</p>
+    <a href="https://www.facebook.com/marketplace/item/980067239172834" class="subtitle">Więcej informacji</a>
+
+  </Block>
+  <Block  filename="gorna0788.jpg"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gsap from 'gsap'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block
+  },
+  mounted() {
+    gsap.from('body', {autoAlpha: 0})
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+html {
+  display: grid;
+  width: 100vw;
+  min-height: 100vh;
+  overflow-x: hidden;
 }
+
+
+*, *:after, *:before {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  display: grid;
+  height: 100%;
+  position: relative;
+  justify-content: center;
+  align-content: center;
+  background: rgb(63, 94, 251);
+  background: linear-gradient(90deg, rgba(63, 94, 251, 1) 0%, rgba(252, 70, 107, 1) 100%);
+}
+
+.wrapper {
+  position: relative;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+}
+
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="block">
+    <div class="texts">
+      <p>{{ title }} </p>
+      <slot></slot>
+    </div>
+    <img class="full" :src="require(`@/assets/img/${filename}`)" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    filename: String,
+    title: String,
+  }
+}
+</script>
+
+<style lang="scss">
+p {
+  font-size: clamp(1.8rem, 10vw, 10rem);
+  background-image: linear-gradient(45deg, #fafaf9, #f1e7ea);
+  background-size: 100%;
+  background-repeat: repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
+
+
+}
+
+.block {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+
+  &--bottom-left {
+    align-content: end;
+    justify-content: start;
+    padding-bottom: 0;
+    padding-left: 0;
+  }
+
+  &--top-left {
+    align-content: start;
+    justify-content: start;
+    padding-top: 0;
+    padding-left: 0;
+  }
+}
+
+.texts {
+  z-index: 1;
+}
+
+#app > div:nth-child(1) > img {
+  clip-path: polygon(20% 0%, 100% 0, 100% 20%, 100% 100%, 80% 100%, 50% 100%, 0 70%, 0 0);
+
+
+}
+
+.full {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100%;
+  object-fit: cover;
+  //mix-blend-mode: soft-light;
+  clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 25% 50%, 0% 0%);
+  pointer-events: none;
+}
+
+.subtitle {
+  font-size: clamp(2.4rem, 7.5vw, 5rem);
+
+}
+
+
+</style>
